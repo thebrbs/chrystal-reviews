5774781693b337ba30ad3e2aae1f5b4d2b1ff767
Chrystal Zou <chrystalzou@gmail.com>
2018-06-06 08:58:42 -0700

    Removed duplicate line to send HTML file

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '../../../index.html'));
-    res.sendFile(path.join(__dirname + '../../../index.html'));
 });
 
 app.get('/reviews', (req, res) => {
