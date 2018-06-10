9d683b025ea98c5a343def9fc8ea3ecaa327d849
Chrystal Zou <chrystalzou@gmail.com>
2018-06-09 19:11:49 -0700

    Front-end functionality for Overview and Summary

--- client/components/overview/Overview.jsx
+++ client/components/overview/Overview.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import Summary from './Summary.jsx';
 import StarDistribution from './StarDistribution.jsx';
 import LovedFor from './LovedFor.jsx';
+import exampleData from './../exampleData.js';
+import overview from './helperFunctions.jsx';
 
 class Overview extends React.Component {
   constructor(props) {
@@ -10,22 +13,37 @@ class Overview extends React.Component {
 
     this.state = {
       totalReviews : 1381,
-      location : 'SOMA'
-    };
+      neighborhood : 'SOMA',
+      overallRating : overview.overallRating(exampleData),
+      foodRating : overview.foodRating(exampleData),
+      serviceRating : overview.serviceRating(exampleData),
+      ambienceRating : overview.ambienceRating(exampleData),
+      valueRating : overview.valueRating(exampleData),
+      noiseLevel : overview.noiseLevel(overview.noise(exampleData)),
+      recommended : overview.recommended(exampleData)
+    }
   }
 
+
   render() {
+    axios.get('/reviews')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     return (
       <div>
         <div>
           <h2>What {this.state.totalReviews} People Are Saying</h2>
         </div>
         <hr></hr>
-        <div><Summary/></div>
+        <div><Summary restaurant={this.state}/></div>
         <div><StarDistribution/></div>
         <div><LovedFor/></div>
         <div>
-          <h3>Best Restaurants in {this.state.location}</h3>
+          <h3>Best Restaurants in {this.state.neighborhood}</h3>
         </div>
       </div>  
     );
--- client/components/overview/Summary.jsx
+++ client/components/overview/Summary.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 
 const Summary = (props) => (
   <div>
-    <div>Overall ratings and reviews</div>
+    <div><h2>Overall ratings and reviews</h2></div>
     <div>Reviews can only be made by people who have eaten at this restaurant</div>
-    <div>Overall star rating here</div>
+    <div>{props.restaurant.overallRating} based on recent ratings</div>
     <div>Individual Ratings here</div>
-      <div>Food Rating</div>
-      <div>Service Rating</div>
-      <div>Ambience Rating</div>
-      <div>Value Rating</div>
-    <div>Noise Level</div>
-    <div>Recommended</div>
+      <div>{props.restaurant.foodRating} Food</div>
+      <div>{props.restaurant.serviceRating} Service</div>
+      <div>{props.restaurant.ambienceRating} Ambience</div>
+      <div>{props.restaurant.valueRating} Value</div>
+    <div>Noise: {props.restaurant.noiseLevel}</div>
+    <div>{props.restaurant.recommended} % of people would recommend it to a friend</div>
   </div>
 );
 
