f2e1b79cbd2c23bcc3fc279644a9435143979851
Chrystal Zou <chrystalzou@gmail.com>
2018-06-11 12:47:01 -0700

    Refactored code to use dynamic data instead of static example data

---
+++ client/components/overview/helperFunctions.jsx
@@ -0,0 +1,50 @@
+const overallRating = (data) => ((data.map(review => review.overall_rating).reduce((a, b) => a + b)) / data.length).toFixed(1)
+
+const foodRating = (data) => ((data.map(review => review.food_rating).reduce((a, b) => a + b)) / data.length).toFixed(1)
+
+const serviceRating = (data) => ((data.map(review => review.service_rating).reduce((a, b) => a + b)) / data.length).toFixed(1)
+
+const ambienceRating = (data) => ((data.map(review => review.ambiance_rating).reduce((a, b) => a + b)) / data.length).toFixed(1)
+
+const valueRating = (data) => ((data.map(review => review.value_rating).reduce((a, b) => a + b)) / data.length).toFixed(1)
+
+const noise = (data) => Math.round(((data.map(review => review.noise_level).reduce((a, b) => a + b)) / data.length));
+
+const noiseLevel = (noise) => {
+  let noiseLevelInWords;
+  if (noise === 1) {
+    noiseLevelInWords = 'Do Not Recall';
+  }
+  else if (noise === 2) {
+    noiseLevelInWords = 'Quiet';
+  }
+  else if (noise === 3) {
+    noiseLevelInWords = 'Moderate';
+  }
+  else if (noise === 4) {
+    noiseLevelInWords = 'Energetic';
+  }
+  return noiseLevelInWords;
+}
+
+const recommended = (data) => {
+  let recommended = 0;
+  for (var i = 0; i < data.length; i++) {
+    recommended += data[i].recommended
+  }
+  return (recommended / data.length * 100).toFixed(1);
+}
+
+module.exports = {
+  overallRating : overallRating,
+  foodRating : foodRating,
+  serviceRating : serviceRating,
+  ambienceRating : ambienceRating,
+  valueRating : valueRating,
+  noise : noise,
+  noiseLevel : noiseLevel,
+  recommended : recommended
+};
+
+const overview = module.exports;
+export default overview;
--- client/components/reviews/ReviewListEntry.jsx
+++ client/components/reviews/ReviewListEntry.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import StarRatings from 'react-star-ratings';
 
-const ReviewListEntry = (props) => (
-  <div>
+const ReviewListEntry = props => (
+  <div className="reviewListEntry">
     <div>Diner info here</div>
-      <div class="profilePhoto"></div>
-      <div class="dinerInfo">
-        <div>author</div>
-        <div>rating</div>
+      <div className="profilePhoto"></div>
+      <div className="dinerInfo">
+        <div>OpenTable Diner {props.review.username} location</div>
+        <div>
+          <StarRatings
+            rating={Number(props.review.overall_rating)}
+            starRatedColor="red"
+            numberOfStars={5}
+            starDimension="20px"
+            starSpacing="1px"
+          />
+        </div>
+        <div>{props.review.overall_rating}</div>
+        <div>{props.review.date}</div>
+        <div>{props.review.body}</div>
+        <a>+ Read More</a>
+        <a><div><i></i>Report</div></a>
+        <a><div><i></i>Helpful</div></a>
+        <hr></hr>
       </div>
   </div>
 );
