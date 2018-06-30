d34db759d8fdb2e7350c87c85dcba27c8c50c494
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-06-30 10:57:29 -0700

    added new relic and redis and edited post route

--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,48 @@
-const port = 8081;
+require('newrelic');
 const bodyParser = require('body-parser');
 const db = require('../../database/index.js');
 const path = require('path');
 const express = require('express');
+const redis = require('redis');
+
+const client = redis.createClient();
 const app = express();
+const port = 8081;
+
+const cache = (req, res, next) => {
+  const restaurant = req.params.restaurantId;
+  client.get(`restaurant/${restaurant}`, (err, data) => {
+    if (err) throw err;
+    if (data != null) {
+      res.send(JSON.parse(data));
+    } else {
+      next();
+    }
+  });
+};
 
 app.use(bodyParser.json());
 
-app.use("/", function(req, res, next) {
-  console.log("the request URL is " + req.url);
-  next();
-});
+app.use('/restaurant/:restaurantId', express.static(path.resolve(__dirname, '../../client')));
 
-app.use('/reviewsBundle.js', express.static(path.join(__dirname + '../../../client/dist/bundle.js')));
-app.use('/reviewsMain.css', express.static(path.join(__dirname + '../../../client/styles/main.css')));
-
-app.get('/restaurant/:restaurantId/reviews', (req, res) => {
-  db.getRestaurantReviews(req.params.restaurantId, (err, results) => {
+app.get('/restaurant/:restaurantId/reviews', cache, (req, res) => {
+  const restaurant = req.params.restaurantId;
+  db.getRestaurantReviews(restaurant, (err, results) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(200).send(results);
+      res.status(200).send(results.rows);
+      client.setex(`restaurant/${restaurant}`, 600, JSON.stringify(results.rows));
     }
   });
 });
 
 app.post('/restaurant/reviews', (req, res) => {
-  db.addReview(req.params, (err, results) => {
+  db.addReview(req.body, (err, results) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(201);
+      res.status(201).send();
     }
   });
 });
@@ -55,4 +67,5 @@ app.delete('/restaurant/:restaurantId/reviews', (req, res) => {
   });
 });
 
+client.on('connect', () => console.log('redis connected'));
 app.listen(port, () => console.log(`CavaTable is listening on port ${port}`));
