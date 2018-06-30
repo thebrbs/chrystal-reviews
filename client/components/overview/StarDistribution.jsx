f5fdf38ca0fdf72f713d3f57d7cbc88d73299b3b
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-06-30 10:46:33 -0700

    edited for style guide

--- client/components/overview/StarDistribution.jsx
+++ client/components/overview/StarDistribution.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import overview from './helperFunctions.jsx';
 
-const StarDistribution = props => {
+const StarDistribution = (props) => {
   const starPercentage = [
     (props.restaurant.fiveStarReviews / props.restaurant.totalReviews) * 100,
     (props.restaurant.fourStarReviews / props.restaurant.totalReviews) * 100,
     (props.restaurant.threeStarReviews / props.restaurant.totalReviews) * 100,
     (props.restaurant.twoStarReviews / props.restaurant.totalReviews) * 100,
-    (props.restaurant.oneStarReviews / props.restaurant.totalReviews) * 100
+    (props.restaurant.oneStarReviews / props.restaurant.totalReviews) * 100,
   ];
   return (
-  <div className="starDistribution">
-    <ul>
-      {starPercentage.map((stars, i) => {
-        return (<li className="reviewUpdateParameter">
-          <span className="starValue">{5 - i}</span>
-          <div className="starBar">
-            <progress value={stars} max="100">50 %</progress>
-          </div>
-        </li>)
-      }
-      )}
-    </ul>
-  </div>
-);}
+    <div className="starDistribution">
+      <ul>
+        {starPercentage.map((stars, i) => (
+          <li className="reviewUpdateParameter" key={i}>
+            <span className="starValue">{5 - i}</span>
+            <div className="starBar">
+              <progress value={stars} max="100">50 %</progress>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default StarDistribution;
